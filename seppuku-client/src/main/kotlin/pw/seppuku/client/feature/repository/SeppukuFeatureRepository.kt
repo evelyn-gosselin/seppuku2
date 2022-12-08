cd734ed94b6675c4c3936aab3f0f4265424ff657
Ossian Winter <ossian@hey.com>
2022-12-08 20:26:25 +0100

    Transform backingMap of SeppukuFeatureRepository to SortedMap

--- seppuku-client/src/main/kotlin/pw/seppuku/client/feature/repository/SeppukuFeatureRepository.kt
+++ seppuku-client/src/main/kotlin/pw/seppuku/client/feature/repository/SeppukuFeatureRepository.kt
@@ -1,5 +1,18 @@
 package pw.seppuku.client.feature.repository
 
+import net.minecraft.client.MinecraftClient
+import pw.seppuku.feature.Feature
 import pw.seppuku.feature.repository.FeatureRepository
 
-class SeppukuFeatureRepository : FeatureRepository()
+class SeppukuFeatureRepository(
+    backingMap: MutableMap<String, Feature> = mutableMapOf()
+) : FeatureRepository(
+    backingMap.toSortedMap(FEATURE_KEY_LENGTH_COMPARATOR)
+) {
+
+    companion object {
+        private val FEATURE_KEY_LENGTH_COMPARATOR = Comparator.comparingInt<String> {
+            MinecraftClient.getInstance().textRenderer.getWidth(it)
+        }
+    }
+}
--- seppuku-sdk/feature-system/src/main/kotlin/pw/seppuku/feature/repository/FeatureRepository.kt
+++ seppuku-sdk/feature-system/src/main/kotlin/pw/seppuku/feature/repository/FeatureRepository.kt
@@ -3,4 +3,6 @@ package pw.seppuku.feature.repository
 import pw.seppuku.feature.Feature
 import pw.seppuku.repository.repositories.SimpleRepository
 
-abstract class FeatureRepository : SimpleRepository<String, Feature>()
+abstract class FeatureRepository(
+    backingMap: MutableMap<String, Feature> = mutableMapOf()
+) : SimpleRepository<String, Feature>(backingMap)
